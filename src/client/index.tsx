05780023c7b5e2ed9fbdadb83d320b16bf898187
Logan Murphy <nagolyhprum@hotmail.com>
2019-01-11 01:54:52 -0500

    add support for generators and async

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,3 +1,5 @@
+import 'babel-polyfill';
+
 import React from "react";
 import ReactDom from "react-dom";
 
@@ -6,6 +8,15 @@ import { applyMiddleware, createStore } from "redux";
 
 import Total from "./components/total";
 import reducers from "./reducers";
+
+function* gen() {
+  yield* ["a", "b", "c"];
+}
+
+async function somethingElse() {
+  const value = await Promise.resolve(true);
+  console.log(value);
+}
 
 const store = createStore(reducers);
 
