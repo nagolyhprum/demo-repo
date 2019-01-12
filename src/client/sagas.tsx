b6ac2ce529b0d0d277f6c5aa074376d6cd2d4ff2
Logan Murphy <lmurphy@learnvest.com>
2019-01-11 19:13:50 -0500

    add some understanding for testing sagas

--- src/client/sagas.tsx
+++ src/client/sagas.tsx
@@ -2,21 +2,25 @@
 //   delay
 // } from 'redux-saga'
 import {
-  // call,
   all,
+  call,
   // takeEvery,
   put,
   takeLatest,
 } from "redux-saga/effects";
+
+const getList = () => new Promise((resolve) => {
+  resolve(["a", "b", "c"]);
+});
 
 import { LIST_FETCH_FAILED, LIST_FETCH_REQUEST, LIST_FETCH_SUCCEEDED } from "./actions/total";
 
 // worker Saga: will be fired on USER_FETCH_REQUESTED actions
 function* fetchList() { // action
    try {
-      const list = yield new Promise((resolve) => {
-        resolve([]);
-      });
+      const list = yield call(getList);
+      // iterator.next('bar');
+      // iterator.throw(new Error('bar'));
       yield put({type: LIST_FETCH_SUCCEEDED, list });
    } catch (e) {
       yield put({type: LIST_FETCH_FAILED, message: e.message});
